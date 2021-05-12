7a253cb7f28f5f4d681c599751827b9ee589d5b8
Mani <manishtulabandu@gmail.com>
2021-05-12 10:55:05 +0530

    first commit
    uploadig all files

---
+++ wipro/src/wipro/One.java
@@ -0,0 +1,21 @@
+package wipro;
+
+import java.util.*;
+public class One {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    if(a>0){
+	        System.out.println("Positive");
+	    }
+	    else if(a==0)
+	    {
+	        System.out.println("Zero");
+	    }
+	    else
+	    {
+	        System.out.println("Negative");
+	    }
+	
+	}
+}
---
+++ wipro/src/wipro/Second.java
@@ -0,0 +1,17 @@
+package wipro;
+
+import java.util.*;
+public class Second {
+public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    if(a%2==0){
+	        System.out.println("even");
+	    }
+	    else
+	    {
+	        System.out.println("Odd");
+	    }
+	
+	}
+}
