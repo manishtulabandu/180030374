89ffe8c21fc8883189665983d435fc493a39f8df
Mani <manishtulabandu@gmail.com>
2021-05-20 12:23:12 +0530

    upload

---
+++ wipro/src/Strings/String4.java
@@ -0,0 +1,11 @@
+package Strings;
+
+import java.lang.*;
+public class String4 {
+public static void main(String[] args)
+{
+   String main_string = "Tomcat";    
+   System.out.println(main_string.substring(0, main_string.length()/2));	
+ } 
+}
+
